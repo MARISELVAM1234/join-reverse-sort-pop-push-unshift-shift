79d4731041497a7c1ebaa12b856138e7a570dff0
selvam <selvamplkm111@gmail.com>
2022-06-21 21:19:39 +0530

    add-folder

---
+++ script.js
@@ -0,0 +1,45 @@
+// join()
+// var a = [1,2,3,4,5];
+// var b = a.join("-");
+// console.log(b);
+// console.log(b.length)
+
+
+
+// reverse()
+// var number = [20,10,7,8,30,100,200,150,300,230,1000,45]; 
+// var arrange = number.reverse();
+// console.log(arrange)
+
+
+
+// sort()
+// var number = ["green","red", "blue", "orange"];
+// var arrange = number.sort();
+// console.log(arrange)
+// var a = [10,20,299,12,14,1000,200,900,300]
+// var b = a.sort()
+// console.log(b)
+
+// push()  to add  the element at the end
+// var a = [10,20,30,40,50,60,70,80,90,100];
+// var b = a.push(110);
+// console.log("To add the the last element :", a)
+
+
+//  unshift() to add the element at the begining
+// var  a = [20,30,40,50,60,70,80,90,100] ;
+// var b = a.unshift(10);
+// console.log("to add the begining of the element :",a)
+
+
+// pop to remove the last element of an array
+var a = [100,200,300,400,500,600,700,800,900];
+var b = a.pop();
+console.log("To remove the last element :",a);
+
+
+// shift to remove the first element of an array
+//  var a = [100,200,300,400,500,600,700,800,900];
+//  var b = a.shift();
+//  console.log("To remove the first element :",a);
